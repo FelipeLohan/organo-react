b51147feb6fd10bdee241d96a2846512c47fb546
Felipe Lohan <felipelohan03052006@gmail.com>
2024-12-19 10:04:52 -0300

    Banner width bug fixed

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,10 +1,15 @@
 import BannerImg from '../../assets/images/banner.png';
+import styled from 'styled-components';
+
+const BannerIMG = styled.img`
+  width: 100%;
+`
 
 export const Banner = () => {
   return(
     <>
     <header>
-      <img src={BannerImg}/>
+      <BannerIMG src={BannerImg}/>
     </header>
     </>
   )
